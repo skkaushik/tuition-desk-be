6c10edcf8d4033dece1831d62f454b8deb5f04ea
Sachin-Zaggle <sachin.kaushik@zaggle.in>
2025-04-20 15:37:18 +0530

    MongoDb Connnection SetUp

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
 
 const app = express();
 
 const SERVER_PORT = 8080;
+
+// body parser middleware
+app.use(express.json());
+app.use(cors());
+
+//mongodb connection
+connectDB();
+
+app.get('/', (req, res) => {
+    res.send('Tuition Desk API is running...');
+});
 
 app.listen(SERVER_PORT, () => {
     console.log(`Tuition Desk Server Running on ${SERVER_PORT}`);
